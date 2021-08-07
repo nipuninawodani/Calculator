8440daaa93fe57c1109e1f5c8d502ec157ca3990
nipuninawodani <61185310+nipuninawodani@users.noreply.github.com>
2021-08-08 00:30:03 +0530

    read from the file is implimented

--- src/java/com.kelaniya.uni/HelloWorld.java
+++ src/java/com.kelaniya.uni/HelloWorld.java
@@ -1,8 +1,23 @@
 package com.kelaniya.uni;
 
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            File myObj = new File("Calculator/src/java/com.kelaniya.uni/numbers.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 
 
