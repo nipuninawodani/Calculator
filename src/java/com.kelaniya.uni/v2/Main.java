aabc72c07774ba40aca28b4a1a44aca428de08f7
nipuninawodani <61185310+nipuninawodani@users.noreply.github.com>
2021-08-22 21:00:10 +0530

    v2 final code

--- src/java/com.kelaniya.uni/v2/Main.java
+++ src/java/com.kelaniya.uni/v2/Main.java
@@ -1,15 +1,41 @@
 package v2;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         //make sure to validate the arguments before using..
+        if (args.length == 0) {
+            System.out.println("Please provide the operation as an argument");
+            return;
+        }
 
-       if(args.length==0) {
-           System.out.println("Please provide the operation as an argument");
-           return;
-       }
-       System.out.println(args[0]);
+        String operator = args[0];
 
+        if (!((operator.equals("add") || (operator.equals("sub") || (operator.equals("mul"))) {
+            System.out.println("Please provide ass, sub or mul as the operator argument");
+            return;
+        }
+
+        //read the numbers from the txt file
+        List<String> numbersStrs = Files.readAllLines(
+                Paths.get("Calculator/src/java/com.kelaniya.uni/v1/numbers.txt")
+        );
+
+        double number1 = Double.parseDouble(numbersStrs.get(0));
+        double number2 = Double.parseDouble(numbersStrs.get(1));
+
+        double result = 0;
+
+        if (operator.equals("add")) {
+            result = number1 + number2;
+        } else if (operator.equals("sub")) {
+            result = number1 - number2;
+        } else if (operator.equals("mul")) {
+            result = number1 * number2;
+        }
+        System.out.println("the result is" + result);
     }
 }
